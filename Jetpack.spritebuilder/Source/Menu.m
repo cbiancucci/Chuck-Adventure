567aa5ab36792291e7c132a804afbcfcbae3b503
cbiancucci <christian.biancucci@mercadolibre.com>
2014-11-27 00:26:45 -0300

    Start Game touch implemented.

--- Jetpack.spritebuilder/Source/Menu.m
+++ Jetpack.spritebuilder/Source/Menu.m
@@ -44,8 +44,37 @@
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-	[audio stopAllEffects];
-	[[CCDirector sharedDirector] replaceScene:[CCBReader loadAsScene:@"MainScene"]];
+	CGPoint touchPosition = [touch locationInView:[touch view]];
+
+	if ([self startGameIsPressed:touchPosition]) {
+		[audio stopAllEffects];
+		[[CCDirector sharedDirector] replaceScene:[CCBReader loadAsScene:@"MainScene"]];
+	}
+	else if ([self optionIsPressed:touchPosition]) {
+	}
+	else if ([self aboutIsPressed:touchPosition]) {
+	}
+}
+
+- (BOOL)startGameIsPressed:(CGPoint)touch {
+	float left = startGameText.position.x - startGameText.contentSize.width / 2;
+	float right = startGameText.position.x + startGameText.contentSize.width / 2;
+
+	float down = (startGameText.position.y - startGameText.contentSize.height / 2) - 20;
+	float up = (startGameText.position.y + startGameText.contentSize.height / 2) - 20;
+
+	if (touch.x >= left && touch.x <= right && touch.y >= down && touch.y <= up) {
+		return YES;
+	}
+	return NO;
+}
+
+- (BOOL)optionIsPressed:(CGPoint)touch {
+	return NO;
+}
+
+- (BOOL)aboutIsPressed:(CGPoint)touch {
+	return NO;
 }
 
 @end
